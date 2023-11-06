73ef4df7210f6d7a9c963ef80b5fcf84a615ff2e
Nirmal Patel <nirmalpatel2312007@gmail.com>
2023-11-06 18:11:39 +0530

    SetUp GitHub-Context

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,22 +1,27 @@
 import React from "react";
-import { useEffect } from "react";
+import { useEffect, useContext } from "react";
+import Spinner from "../layout/Spinner";
+import UserItem from "./UserItem";
+import GithubContext from "../../context/github/GithubContext";
 
 function UserResults() {
+  const { users, loading, fetchUsers } = useContext(GithubContext);
+
   useEffect(() => {
     fetchUsers();
   }, []);
 
-  const fetchUsers = async () => {
-    const response = await fetch(`${import.meta.env.VITE_GITHUB_URL}/users`, {
-      headers: {
-        Authorization: "token ${import.meta.env.VITE_GITHUB_TOKEN}",
-      },
-    });
-    const data = await response.json();
-    console.log(data);
-  };
-
-  return <div>UserResults</div>;
+  if (!loading) {
+    return (
+      <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+        {users.map((user) => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    );
+  } else {
+    return <Spinner />;
+  }
 }
 
 export default UserResults;
