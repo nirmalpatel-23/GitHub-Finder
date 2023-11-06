1f003738258eb3794584972c9f7fb6c9b0770beb
Nirmal Patel <nirmalpatel2312007@gmail.com>
2023-11-06 15:22:12 +0530

    UserResults Updated

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import UserResults from "../components/users/UserResults";
+
+function Home() {
+  console.log(import.meta.env);
+  return (
+    <>
+      <UserResults />
+    </>
+  );
+}
+
+export default Home;
